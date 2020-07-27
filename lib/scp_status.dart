c26d37929e4210ee512045f8c34072e1a3d0aa2a
Benjamin Schilling <benjamin.schilling33@gmail.com>
2020-07-27 15:15:10 +0200

    Update missing license headers

--- lib/scp_status.dart
+++ lib/scp_status.dart
@@ -1,3 +1,10 @@
+/*
+secure_control_protocol
+ScpStatus Class
+SPDX-License-Identifier: GPL-3.0-only
+Copyright (C) 2020 Benjamin Schilling
+*/
+
 class ScpStatus {
   static const String RESULT_DONE = "done";
   static const String RESULT_SUCCESS = "success";
